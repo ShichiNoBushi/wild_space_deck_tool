96f5acd7119d0c93d1409c408f9265aa09ce5f56
ShichiNoBushi <shichinobushi@gmail.com>
2025-01-12 16:41:33 -0600

    Added tabs and character display and creation functions.

--- deck_tool.py
+++ deck_tool.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 
 from deck import *
@@ -21,22 +22,27 @@ class Window:
         self._root = tk.Tk()
         self._root.title = "Wild Space Deck Tool"
 
-        #self._label = tk.Label(self._root, text = "Hello world!")
-        #self._label.pack()
-
-        #self._button = tk.Button(self._root, text = "Click me!", command = self.on_button_click)
-        #self._button.pack()
-
         self.galaxy_a_deck = Deck()
         self.galaxy_a_deck.fillStandardDeck()
         self.galaxy_a_deck.shuffle()
         self.galaxy_d_deck = self.galaxy_a_deck.cut()
         self.galaxy_discard = Deck()
 
-        self.draw_card_button = tk.Button(self._root, text = "Draw Card", command = self.draw_card)
+        self.characters = {}
+
+        self.main_nb = ttk.Notebook(self._root)
+
+        self.button_tab = tk.Frame(self.main_nb)
+        self.characters_tab = tk.Frame(self.main_nb)
+        self.create_tab = tk.Frame(self.main_nb)
+        self.main_nb.add(self.button_tab, text = "Buttons")
+        self.main_nb.add(self.characters_tab, text = "Characters")
+        self.main_nb.add(self.create_tab, text = "Create")
+
+        self.draw_card_button = tk.Button(self.button_tab, text = "Draw Card", command = self.draw_card)
         self.draw_card_button.pack()
 
-        self.draw_hand_frame = tk.Frame(self._root)
+        self.draw_hand_frame = tk.Frame(self.button_tab)
 
         self.hand_size = tk.IntVar()
         self.hand_entry = tk.Entry(self.draw_hand_frame, textvariable = self.hand_size, width = 5)
@@ -46,6 +52,42 @@ class Window:
         self.draw_hand_button.pack(side = tk.RIGHT)
     
         self.draw_hand_frame.pack()
+
+        self.char_list_frame = tk.Frame(self.characters_tab)
+
+        self.char_list_scroll = tk.Scrollbar(self.char_list_frame)
+        self.char_list_scroll.pack(side = tk.RIGHT, fill = tk.Y)
+
+        self.lb_characters = tk.StringVar(value = list(self.characters.keys()))
+        self.characters_list = tk.Listbox(self.characters_tab, selectmode = tk.SINGLE, listvariable = self.lb_characters, yscrollcommand = self.char_list_scroll.set)
+        self.characters_list.pack(side = tk.LEFT, fill = tk.Y)
+
+        self.char_list_scroll.config(command = self.characters_list.yview)
+
+        self.char_list_frame.pack(side = tk.LEFT)
+
+        self.char_data_frame1 = tk.Frame(self.characters_tab)
+        self.char_data_frame2 = tk.Frame(self.char_data_frame1)
+
+        self.char_data_scroll = tk.Scrollbar(self.char_data_frame2)
+        self.char_data_scroll.pack(side = tk.RIGHT, fill = tk.Y)
+
+        self.characters_data = tk.Text(self.char_data_frame2, wrap = tk.WORD, yscrollcommand = self.char_data_scroll.set, state = tk.DISABLED)
+        self.characters_data.pack(side = tk.LEFT, fill = tk.Y)
+
+        self.char_data_scroll.config(command = self.characters_data.yview)
+
+        self.char_data_frame2.pack(side = tk.TOP)
+
+        self.char_data_button = tk.Button(self.char_data_frame1, text = "Display Character", command = self.display_character)
+        self.char_data_button.pack(side = tk.BOTTOM)
+
+        self.char_data_frame1.pack(side = tk.RIGHT)
+
+        self.create_button = tk.Button(self.create_tab, text = "Create Character", command = self.create_character_simple)
+        self.create_button.pack()
+
+        self.main_nb.pack(side = tk.TOP)
 
         self.log_frame = tk.Frame(self._root)
 
@@ -118,11 +160,27 @@ class Window:
         except tk.TclError:
             self.update_log("Invalid entry: enter positive whole number")
 
+    def display_character(self):
+        if len(self.characters_list.curselection()) == 0:
+            self.update_log("No character selected.")
+        else:
+            self.update_log("Display character function not yet implemented")
+
+    def create_character_simple(self):
+        new_name = f"Character {len(self.characters)}"
+        new_character = Character(new_name)
+        self.characters[new_name] = new_character
+        self.update_log(f"Character {new_name} added.")
+        self.update_listbox()
+
     def update_log(self, message):
         self.log_text.config(state = tk.NORMAL)
         self.log_text.insert(tk.END, message + '\n')
         self.log_text.see(tk.END)
         self.log_text.config(state = tk.DISABLED)
+
+    def update_listbox(self):
+        self.lb_characters.set(list(self.characters.keys()))
 
     def close(self):
         #self._running = False
